4d344fc3141b6547d2fda07dd46385613e6e58f1
DysonTilano <dysontilano@gmail.com>
2022-09-04 20:50:12 -0500

    Added source code files

---
+++ src/main/java/com/project/dev/Application.java
@@ -0,0 +1,35 @@
+/*
+ * @fileoverview {Application} se encarga de realizar tareas especificas.
+ *
+ * @version             1.0
+ *
+ * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
+ * Copyright (C) Dyson Parra
+ *
+ * @History v1.0 --- La implementacion de {Application} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {Application} fue escrita por Dyson A. Parra T.
+ */
+package com.project.dev;
+
+import com.project.dev.key.listener.KeyListenerFrame;
+
+/**
+ * TODO: Definición de {@code Application}.
+ *
+ * @author Dyson Parra
+ * @since 1.8
+ */
+public class Application {
+
+    /**
+     * Entrada principal del sistema.
+     *
+     * @param args argumentos de la linea de comandos.
+     */
+    public static void main(String[] args) {
+        KeyListenerFrame keyListenerFrame = new KeyListenerFrame();
+        keyListenerFrame.setVisible(true);
+        keyListenerFrame.setDefaultCloseOperation(KeyListenerFrame.EXIT_ON_CLOSE);
+    }
+
+}
---
+++ src/main/java/com/project/dev/key/listener/KeyListenerConstant.java
@@ -0,0 +1,30 @@
+/*
+ * @fileoverview {KeyListenerConstant} se encarga de realizar tareas especificas.
+ *
+ * @version             1.0
+ *
+ * @author              Dyson Arley Parra Tilano <dysontilano@gmail.com>
+ * Copyright (C) Dyson Parra
+ *
+ * @History v1.0 --- La implementacion de {KeyListenerConstant} fue realizada el 31/07/2022.
+ * @Dev - La primera version de {KeyListenerConstant} fue escrita por Dyson A. Parra T.
+ */
+package com.project.dev.key.listener;
+
+/**
+ * TODO: Definición de {@code KeyListenerConstant}.
+ *
+ * @author Dyson Parra
+ * @since 1.8
+ */
+public interface KeyListenerConstant {
+
+    public static final String RES_ROOT_PATH = "/assets/com/project/dev/key/listener";
+    public static final String PROJ_PATH = "src/main/resources";
+    public static final String PROJ_RES_PATH = PROJ_PATH + RES_ROOT_PATH;
+
+    public static final String KEY_ICON_FILE = "key_listener_icon.png";
+    public static final String KEY_ICON_RES_PATH = RES_ROOT_PATH + "/" + KEY_ICON_FILE;
+    public static final String KEY_ICON_PROJ_PATH = PROJ_RES_PATH + "/" + KEY_ICON_FILE;
+
+}
